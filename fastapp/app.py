6308e4c1a5b13292dc1a9a7dd8cf7716e298ac29
jhonathanwesley <jhonwfdalves@gmail.com>
2025-05-27 02:26:07 -0300

    feat: Adiciona endpoint HTML e melhora a estrutura do código

--- fastapp/app.py
+++ fastapp/app.py
@@ -1,8 +1,17 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/', status_code=200)
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Primeiro endpoint com fastAPI'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def write_html():
+    with open('fastapp/index.html', 'r', encoding='utf-8') as f:
+        html_content = f.read()
+    return HTMLResponse(html_content, HTTPStatus.OK)
